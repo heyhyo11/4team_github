4f9b60a91456c393c793a7a27b63313084bd2134
김호 <91515828+hopaom@users.noreply.github.com>
2022-01-24 16:43:54 +0900

    평균구하기 추가 - 김호

--- 220124/김호.py
+++ 220124/김호.py
@@ -1,16 +1,17 @@
 #없는 숫자 더하기
-
 def solution(numbers):
     row = list(range(0, 10))
     not_in = []
-
     for num in row:
         if num in numbers:
             continue
         else:
             not_in.append(num)
-
     return sum(not_in)
 
 
-print(solution([1,2,4,5,8,9]))
+#평균 구하기
+def solution(arr):
+    num_sum = sum(arr)
+    answer = num_sum/len(arr)
+    return answer
